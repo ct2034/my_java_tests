002ef1862618f9adc1429ddb6d10fc25ccce99a7
ct2034 <post@henkelchristian.de>
2015-10-17 15:21:03 +0200

    a new project

---
+++ TriggerTesting/src/TriggerTesting.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ch
+ *
+ */
+public class TriggerTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("init ...");
+	}
+
+}
